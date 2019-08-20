fe44584491aba31d4caddc025860d90ee32d325a
Arturo Cuevas <26891629+darkarth80@users.noreply.github.com>
2019-08-20 01:02:38 -0500

    Se agrega prueba unitaria del funcionamiento del proceso batch.

--- src/test/java/com/darkarth/spring/boot/batch/demo/SpringBatchDemoSimpleAplicacionTests.java
+++ src/test/java/com/darkarth/spring/boot/batch/demo/SpringBatchDemoSimpleAplicacionTests.java
@@ -1,16 +1,31 @@
 package com.darkarth.spring.boot.batch.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.darkarth.spring.boot.batch.demo.config.ConfiguracionBatchTest;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes={ConfiguracionBatchTest.class})
 public class SpringBatchDemoSimpleAplicacionTests {
 
+	@Autowired
+	private JobLauncherTestUtils jltu;
+
 	@Test
-	public void contextLoads() {
+	public void pruebaDemoSimple() throws Exception {
+		JobExecution je = jltu.launchJob();
+
+		assertThat(je.getExitStatus().getExitCode()).isEqualTo(ExitStatus.COMPLETED.getExitCode());
+
 	}
 
 }
